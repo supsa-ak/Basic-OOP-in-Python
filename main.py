f31263cede4012948eb7585e1bac8a404a01006c
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-14 10:57:38 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+#class
+class Fighters:  #class name
+  def __init__(friendname,surname): #method #parameter
+    friendname.surname = surname #attribute
+
+
+#Object
+name1 = Fighters('Kalpande') #Object name 
+name2 = Fighters('joe')
+  
+print(name1.surname)
+print(name2.surname)
+
