5c425da559d7fc622f1d2fa3f2898e9cdbafaebd
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-15 14:22:05 +0530

    Create OOP 2 new_understanding.py

---
+++ OOP 2 new_understanding.py
@@ -0,0 +1,9 @@
+class Fighters: 
+  def __init__(friendname, a):
+    friendname.surname = a
+
+name1 = Fighters('Kalpande')  
+name2 = Fighters('joe')
+  
+print(name1.surname)
+print(name2.surname)
