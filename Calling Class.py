b0d82da44fa5082f02717562681b25c783c509c8
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-17 11:45:28 +0530

    Create Calling Class.py

---
+++ Calling Class.py
@@ -0,0 +1,18 @@
+class hello:
+#class hello():
+    #def set(g,a):
+    #    g.data = a
+    def __init__ (self,name):
+        self.name = name
+
+    def show(g):
+        print(g.data)
+
+    #print("sak is real")
+
+#hello()
+#we can call class
+
+v= hello("sak")
+print(v.name)
+#not running it requires function init to run
