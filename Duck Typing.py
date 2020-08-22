f564a1469fed12a021804ddb01793f29eca547b8
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-22 09:53:07 +0530

    Create Duck Typing.py

---
+++ Duck Typing.py
@@ -0,0 +1,24 @@
+class Pycharm:
+
+    def execute(self):
+        print("compiling")
+        print("Running")
+
+class Laptop:
+
+    def code(self):
+        Pycharm.execute(jack) # calling method of first class in method of another class
+
+class Packing:
+
+    def aj(self):
+        print('hi')
+
+idle = Pycharm()
+
+lap1 = Laptop()
+
+jack = Packing()
+
+
+lap1.code()
