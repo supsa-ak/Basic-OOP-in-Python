af4d06e8f68b4c63706930affdabed608aaed436
Sarthak Kalpande <65620738+sakisrealop@users.noreply.github.com>
2020-08-24 17:33:07 +0530

    Create Method Overloading.py

---
+++ Method Overloading.py
@@ -0,0 +1,25 @@
+class Student:
+
+    def __init__(self, m1 ,m2):
+        self.m1 = m1
+        self.m2 = m2
+
+    def sum(self,a = None,b = None,c = None):
+
+        s = 0
+
+        if a != None and b != None and c != None :
+            s = a + b + c
+        elif a != None and b != None :
+            s = a + b
+        else :
+            s = a
+
+        return s
+
+s1 = Student(100, 100)
+
+print(s1.sum(1,2,3))
+print(s1.sum(1,2))
+print(s1.sum(1))
+print(s1.sum())
